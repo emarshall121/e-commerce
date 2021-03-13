f1e49ad1dd6ce83e556d6783f3bf5fd2defffaea
EMarshall <emilyjmarshall121@gmail.com>
2021-03-12 20:08:27 -0700

    added models

--- config/connection.js
+++ config/connection.js
@@ -1,7 +1,9 @@
-require('dotenv').config();
-
+// Import sequelize constructor
 const Sequelize = require('sequelize');
 
+require('dotenv').config();
+
+// Create connection to the database
 const sequelize = process.env.JAWSDB_URL
   ? new Sequelize(process.env.JAWSDB_URL)
   : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
